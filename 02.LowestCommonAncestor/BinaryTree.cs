289538733936bf34725373c80e27510e6eb8f28f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-05 13:26:36 +0200

    Heaps and Binary Trees - Exercise

--- 02.LowestCommonAncestor/BinaryTree.cs
+++ 02.LowestCommonAncestor/BinaryTree.cs
@@ -36,6 +36,25 @@
 
         public T FindLowestCommonAncestor(T first, T second)
         {
+            BinaryTree<T> firstNode = this.FindBfs(first, this);
+            BinaryTree<T> secondNode = this.FindBfs(second, this);
+
+            if (firstNode is null || secondNode is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var firstNodeAncestors = this.FindAncestors(firstNode);
+            var secondNodeAncestors = this.FindAncestors(secondNode);
+        }
+
+        private object FindAncestors(BinaryTree<T> firstNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        private BinaryTree<T> FindBfs(T first, BinaryTree<T> binaryTree)
+        {
             throw new NotImplementedException();
         }
     }
